e54aa8bd7b1d2068326b7f72fcd3c1d04cba5da7
Dmitriy Sidorov <d_sidorov@wargaming.net>
2021-03-30 11:26:03 +0300

    Finish signin component (#6 - done)

--- recordstore-front/src/components/Signin.vue
+++ recordstore-front/src/components/Signin.vue
@@ -4,7 +4,6 @@
       <h3 class="text-2xl mb-6 text-grey-darkest">Sign In</h3>
       <form @submit.prevent="signin">
         <div class="text-red" v-if="error">{{ error }}</div>
-        
         <div class="mb-6">
           <label for="email" class="label">E-mail Adress</label>
           <input type="email" v-model="email" class="input" id="email" placeholder="example@gmail.com">
@@ -15,13 +14,56 @@
           <input type="password" v-model="password" class="input" id="password">
         </div>
 
-
-
+        <button type="submit" class="bg-green font-sans font-bold px-4 rounded cursor-pointer no-underline hover:bg-green-dark block w-full py-4 text-white items-center justify-center">Sign In</button>
+        <div class=my-4>
+          <router-link to="/singup" class="link">Sign Up</router-link>
+        </div>
       </form>
     </div>
   </div>
 </template>
 
 <script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+  created () {
+    this.checkSignedIn()
+  },
+  updated () {
+    this.checkSignedIn()
+  },
+  methods: {
+    signin () {
+      this.$http.plain.post('/signin', { email: this.email, password: this.password })
+        .then(response => this.signinSuccessful(response))
+        .catch(error => this.signinFailed(error))
+    },
+    signinSuccessful (response) {
+      if (!response.data.csrf) {
+        this.signinFailed(response)
+        return
+      }
 
+      localStorage.csrf = response.data.csrf
+      localStorage.signedIn = true
+      this.error = ''
+      this.$router.replace('/records')
+    },
+    signinFailed (error) {
+      this.error = (error.data.response && error.response.data && error.response.data.error) || ''
+      delete localStorage.csrf
+      delete localStorage.signedIn
+    },
+    checkSignedIn () {
+      if (localStorage.signedIn) this.$router.replace('/records')
+    }
+  }
+}
 </script>
